9f4fe27bf43df9590d7225427fd00e0a13dd76e1
Nicole Dominguez <sodevious.net@gmail.com>
2025-10-22 20:36:32 -0400

    dont use 11ty fetch

--- _data/events.js
+++ _data/events.js
@@ -1,4 +1,3 @@
-import Fetch from "@11ty/eleventy-fetch";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,10 +5,8 @@ dotenv.config();
 const API_URL = process.env.API_URL;
 
 export default async function () {
-	let json = await Fetch(API_URL, {
-		duration: "1d", // save for 1 day
-		type: "json", // we’ll parse JSON for you
-	});
-    console.log("Fetched events data from API:", json);
+	const response = await fetch(API_URL);
+	const json = await response.json();
+	console.log("Fetched events data from API:", json);
 	return json;
 };
